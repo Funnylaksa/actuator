e3e244bf3bb3d2f365d9f401d431fefc1c9e9c52
Funnylaksa <ivan.lee.ch8@gmail.com>
2022-10-16 13:31:37 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ def on_button_pressed_a():
     basic.pause(100)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_received_string(receivedString):
+    if receivedString == "OFF":
+        # fan off
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        basic.show_icon(IconNames.NO)
+        basic.pause(100)
+    elif receivedString == "ON":
+        # fan on
+        pins.digital_write_pin(DigitalPin.P1, 1)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        basic.show_icon(IconNames.HEART)
+        basic.pause(100)
+    else:
+        pass
+radio.on_received_string(on_received_string)
+
 def on_button_pressed_b():
     # fan off
     pins.digital_write_pin(DigitalPin.P1, 0)
@@ -14,24 +31,12 @@ def on_button_pressed_b():
     basic.pause(100)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-colorbit_51bit = None
-w_moist = 10
-w_weight = 2
-w_temp = 3
-w_humid = 4
-w_wind = 5
-w_heater = -2
-x_moist = 10
-x_weight = 2
-x_temp = 3
-x_humid = 4
-x_wind = 5
-x_heater = 20
-radio.set_group(88)
-radio.set_group(90)
+colorbit_51bit: colorbit.Strip = None
+radio.set_group(60)
 OLED12864_I2C.init(60)
+# radio.on_received_value(on_received_value)
 
-def on_forever2():
+def on_forever():
     global colorbit_51bit
     serial.write_line("rainbow light")
     colorbit_51bit = colorbit.init_color_bit(DigitalPin.P8, BitColorMode.RGB)
@@ -40,5 +45,4 @@ def on_forever2():
     basic.pause(500)
     colorbit_51bit.show_color(colorbit.colors(BitColors.BLUE))
     basic.pause(500)
-
-basic.forever(on_forever2)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -5,6 +5,23 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
     basic.pause(100)
 })
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "OFF") {
+        // fan off
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.showIcon(IconNames.No)
+        basic.pause(100)
+    } else if (receivedString == "ON") {
+        // fan on
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+    } else {
+    	
+    }
+})
 input.onButtonPressed(Button.B, function () {
     // fan off
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -13,21 +30,9 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(100)
 })
 let colorbit_51bit: colorbit.Strip = null
-let w_moist = 10
-let w_weight = 2
-let w_temp = 3
-let w_humid = 4
-let w_wind = 5
-let w_heater = -2
-let x_moist = 10
-let x_weight = 2
-let x_temp = 3
-let x_humid = 4
-let x_wind = 5
-let x_heater = 20
-radio.setGroup(88)
-radio.setGroup(90)
+radio.setGroup(60)
 OLED12864_I2C.init(60)
+// radio.on_received_value(on_received_value)
 basic.forever(function () {
     serial.writeLine("rainbow light")
     colorbit_51bit = colorbit.initColorBit(DigitalPin.P8, BitColorMode.RGB)
