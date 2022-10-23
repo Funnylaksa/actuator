6bb2b365559f1f53cbef910d7be7613d948b978d
Funnylaksa <ivan.lee.ch8@gmail.com>
2022-10-23 13:06:31 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
     basic.showIcon(IconNames.Heart)
-    basic.pause(500)
+    basic.pause(100)
     colorbit_51bit.showColor(colorbit.colors(BitColors.Green))
     serial.writeString("fan on")
 })
@@ -29,12 +29,48 @@ input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
     basic.showIcon(IconNames.No)
-    basic.pause(500)
+    basic.pause(100)
     colorbit_51bit.showColor(colorbit.colors(BitColors.Black))
     serial.writeString("fan off")
+    serial.writeValue("x_humid", x_humid)
+    basic.pause(100)
+    serial.writeValue("x_light", x_light)
+    basic.pause(100)
+    serial.writeValue("x_moist", x_moist)
+    basic.pause(100)
+    serial.writeValue("x_wind", x_wind)
+    basic.pause(100)
+    serial.writeValue("x_temp", x_temp)
+    basic.pause(100)
+    serial.writeValue("x_weight", x_weight)
 })
-let x_wind = 0
+radio.onReceivedValue(function (name, value) {
+    if (name == "humi") {
+        x_humid = value
+        serial.writeValue("x_humid", x_humid)
+    } else if (name == "light") {
+        x_light = value
+        serial.writeValue("x_light", x_light)
+    } else if (name == "mois") {
+        x_moist = value
+        serial.writeValue("x_moist", x_moist)
+    } else if (name == "winds") {
+        x_wind = value
+        serial.writeValue("x_wind", x_wind)
+    } else if (name == "temp") {
+        x_temp = value
+        serial.writeValue("x_temp", x_temp)
+    } else if (name == "weight") {
+        x_weight = value
+        serial.writeValue("x_weight", x_weight)
+    }
+})
 let x_weight = 0
+let x_temp = 0
+let x_wind = 0
+let x_moist = 0
+let x_light = 0
+let x_humid = 0
 let colorbit_51bit: colorbit.Strip = null
 radio.setGroup(60)
 OLED12864_I2C.init(60)
@@ -42,13 +78,5 @@ colorbit_51bit = colorbit.initColorBit(DigitalPin.P8, BitColorMode.RGB)
 colorbit_51bit.showColor(colorbit.colors(BitColors.White))
 basic.showIcon(IconNames.SmallDiamond)
 basic.forever(function () {
-    let x_humid = Math.random()
-x_weight = Math.random() * 10
-    x_wind = Math.random() * 5
-    basic.pause(100)
-    serial.writeValue("x_weight", x_weight)
-    basic.pause(100)
-    serial.writeValue("x_wind", x_wind)
-    basic.pause(100)
-    serial.writeValue("x_humid", x_humid)
+    basic.pause(500)
 })
